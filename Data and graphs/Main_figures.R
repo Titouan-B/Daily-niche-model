1a80c6b87e88161cb0a092e820dd5fdb282ba89a
Titouan B <84088901+Titouan-B@users.noreply.github.com>
2024-06-28 15:48:54 +0200

    Add files via upload

---
+++ Data and graphs/Main_figures.R
@@ -0,0 +1,204 @@
+library(ggplot2)
+library(ggpattern)
+library(truncnorm)
+library(patchwork)
+
+
+# Figure 1: Emergence of sub-populations with different temporal niches depending on the timing of emergence of adults.
+
+NG_e <- read_excel("Data.xlsx", sheet = 'Emergence_NG')
+
+NG_e$Emergence <- as.factor(NG_e$Emergence)
+ggplot(NG_e, aes(fill = Type, y = Percentage, x = Emergence, 
+                pattern = Type)) +
+  geom_bar_pattern(aes(pattern = Type, pattern_angle = Type),
+                   position = "stack", stat = "identity", 
+                   pattern_fill = "black", fill = "white", 
+                   colour = "black", pattern_spacing = 0.02,
+  ) +
+  scale_pattern_manual(values = c('Early' = 'stripe',
+                                  'Late' = 'circle',
+                                  'Bimodal' = 'none')) +
+  scale_pattern_angle_manual(name="Type", values = c(0, 45,45))+
+  theme_classic() +
+  labs(x = "Emergence (e)", y = "Percentage of each\ntype of sub-population")+
+  theme(text = element_text(size = 17)) +
+  theme(legend.position = "none")
+
+
+
+# Create a sequence of x values
+x_valsE <- seq(lower, upper, length.out = 100)
+# Compute the corresponding y values using dtruncnorm
+y_valsE <- dtruncnorm(x_valsE, mean = 0.15, sd = 0.05, a = 0, b = 1)
+
+# Create the plot
+Early <- ggplot(data.frame(x = x_valsE, y = y_valsE), aes(x = x, y = y)) + 
+  geom_vline(xintercept = 0.3, linetype="dashed")+
+  geom_area_pattern(aes(pattern = "stripe"), 
+                    fill = "white", 
+                    pattern_fill = "black", 
+                    pattern_density = 0.1,
+                    pattern_spacing = 0.02,
+                    pattern_size = 0.2,
+                    pattern = "stripe") +
+  stat_function(fun=function(x) dtruncnorm(x, mean = 0.15, sd = 0.05, a = 0, b = 1))+
+  theme_classic() +
+  labs(x = "Day time", y = "Density of active individuals")+
+  theme(text = element_text(size = 22))
+
+
+x_valsL <- seq(lower, upper, length.out = 100)
+y_valsL <- dtruncnorm(x_valsL, mean = 0.5, sd = 0.05, a = 0, b = 1)
+
+Late <- ggplot(data.frame(x = x_valsL, y = y_valsL), aes(x = x, y = y)) + 
+  geom_vline(xintercept = 0.3, linetype="dashed")+
+  geom_area_pattern(aes(pattern = "circle"), 
+                    fill = "white", 
+                    pattern_fill = "black", 
+                    pattern_density = 0.1,
+                    pattern_spacing = 0.02,
+                    pattern_size = 0.2,
+                    pattern = "circle") +
+  stat_function(fun=function(x) dtruncnorm(x, mean = 0.5, sd = sd_val, a = lower, b = upper))+
+  theme_classic() +
+  labs(x = "Day time", y = "")+
+  theme(text = element_text(size = 22))
+
+Bimodal <- ggplot(data.frame(x = c(x_valsL,x_valsE), y = c(y_valsL,y_valsE)), aes(x = x, y = y)) + 
+  geom_vline(xintercept = 0.3, linetype="dashed")+
+  stat_function(fun=function(x) dtruncnorm(x, mean = 0.15, sd = sd_val, a = lower, b = upper) 
+                + dtruncnorm(x, mean = 0.5, sd = sd_val, a = lower, b = upper))+
+  theme_classic() +
+  labs(x = "Day time", y = "")+
+  theme(text = element_text(size = 22))
+
+
+final_plot <- Early + Late + Bimodal+ plot_annotation(tag_levels = (tag_levels = list(c('B', 'C','D'), '1')))
+final_plot
+
+
+# Figure 2: Effect of variance in the timing of adult emergence on the evolution of sub-populations with differentiated temporal niches
+
+NG_ve <- read_excel("Data.xlsx", sheet = 'Var_Emerg_NG')
+
+ggplot(NG_ve, aes(fill = Type, y = Percentage, x = ve, 
+               pattern = Type)) +
+  geom_bar_pattern(aes(pattern = Type, pattern_angle = Type),
+                   position = "stack", stat = "identity", 
+                   pattern_fill = "black", fill = "white", 
+                   colour = "black", pattern_spacing = 0.02,
+  ) +
+  scale_pattern_manual(values = c('Early' = 'stripe',
+                                  'Late' = 'circle',
+                                  'Bimodal' = 'none')) +
+  scale_pattern_angle_manual(name="Type", values = c(0, 45,45))+
+  theme_classic() +
+  labs(x = expression("Variance in the emergence time (" * italic(V[e]) * ")"), y = "Percentage of each\ntype of sub-population")+
+  theme(text = element_text(size = 17)) +
+  theme(legend.position = "none")
+
+# Figure 3: Effects of the male-male competition and male-female asymmetry on the evolution of sub-populations with different temporal niches.
+
+dltc <- read_excel("Data.xlsx", sheet = 'Competition_Bimodal')
+
+dltc$dltc <- dltc$dltc*100
+p1 <- ggplot(dltc, aes(x=dltc, y=Percentage)) +
+  geom_point(size=3) +
+  geom_errorbar(aes(ymin=Percentage-sd100, ymax=Percentage+sd100), width=.1,
+                position=position_dodge(.2)) +
+  scale_x_continuous(trans=scales::pseudo_log_trans(base = 10))+
+  xlab(expression("Strength of male-male competition (" * italic(delta["c"]) * ")")) +ylab("Percentage of bimodal distribution\nafter 500 generations")+
+  ylim(0,100)+
+  theme_classic()+
+  theme(text = element_text(size = 17))
+
+
+Femelles <- read_excel("Data.xlsx", sheet = 'Competition_Females')
+
+Femelles$p <- as.factor(Femelles$p)
+p2 <- ggplot(data=Femelles, aes(x=p,y=bimodality))+
+  geom_point(size=3)+
+  geom_errorbar(aes(ymin=bimodality-sd100, ymax=bimodality+sd100), width=.2,
+                position=position_dodge(.2)) +
+  ylab("")+
+  xlab(expression("Maximum number of mates per female (" * italic(p) * ")"))+
+  ylim(0,100)+
+  theme_classic()+
+  theme(text = element_text(size = 17))
+
+final_plot <- p1 + p2 + plot_annotation(tag_levels = 'A')
+final_plot
+
+
+# Figure 4: Effect of genetic incompatibility and male-male competition on the genetic differentiation between sub-populations with different temporal niche
+
+G <- read_excel("Data.xlsx", sheet = 'G_Fst')
+
+G$G <- as.factor(G$G)
+
+p1 <- ggplot(G, aes(x=G, y=Fst_G)) +
+  geom_point(size = 3) +
+  geom_errorbar(aes(ymin=Fstg_min, ymax=Fstg_max), width=.4,
+                position=position_dodge(0)) +
+  xlab(expression("Genetic threshold of reproductive isolation (" * italic("G") * ")")) + ylab("Average Fst values")+
+  theme_classic()+
+  theme(text = element_text(size = 17))
+
+
+dltc <- read_excel("Data.xlsx", sheet = 'Competition_Fst')
+
+p2 <- ggplot(dltc, aes(x=dltc, y=Fst_dltc)) +
+  geom_point(size =3) +
+  geom_errorbar(aes(ymin=Fstd_min, ymax=Fstd_max), width=.03) +
+  xlab(expression("Strength of male−male competition (" * italic(delta*"c") * ")" )) + ylab("  ")+
+  theme_classic()+
+  theme(text = element_text(size = 17))
+
+
+final_plot <- p1 + p2 + plot_annotation(tag_levels = 'A')
+final_plot
+
+
+# Figure 5: Factors shaping the duration of coexistence between early and late sub-populations
+
+K_c <- read_excel("Data.xlsx", sheet = 'K_Coexistence')
+
+K_c$K <- as.factor(K_c$K)
+
+p1 <- ggplot(K_c, aes(x=K, y=Elen, group = 1)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=Elen-sd, ymax=Elen+sd), width=.2) +
+  theme_classic()+
+  theme(text = element_text(size = 15))+
+  ylab("Average coexistence duration (in days)")+
+  xlab(expression("Carrying capacity of the environment (" * italic(K) * ")"))
+
+
+G_c <- read_excel("Data.xlsx", sheet = 'G_Coexistence')
+
+G_c$G <- as.factor(G_c$G)
+
+p2 <- ggplot(G_c, aes(x=G, y=Elength, group = 1)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=Elength-sd_L, ymax=Elength+sd_L), width=.2) +
+  theme_classic()+
+  theme(text = element_text(size = 15))+
+  ylab(" ")+
+  xlab(expression("Genetic threshold of reproductive isolation (" * italic(G) * ")"))
+
+
+dltc_c <- read_excel("Data.xlsx", sheet = 'Competition_Coexistence')
+
+p3 <- ggplot(dltc_c, aes(x=dltc, y=Elength)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=yminEL, ymax=ymaxEL), width=.05) +
+  scale_x_continuous(trans=scales::pseudo_log_trans(base = 10))+
+  ylab(" ")+
+  xlab(expression("Strength of male-male competition (" * italic(delta["c"]) * ")"))+
+  theme_classic()
+  
+  
+final_plot <- p1 + p2 + p3 + plot_annotation(tag_levels = 'A')
+final_plot
+
---
+++ Data and graphs/Supplementary_figures.R
@@ -0,0 +1,154 @@
+library(ggplot2)
+library(ggpattern)
+library(truncnorm)
+library(patchwork)
+
+# Figure S 1: Comparisons of simulation outcomes, after 500 vs. 2000 generations.
+
+
+Duration <- read_excel("Supplementary_data.xlsx", sheet = '500vs2000')
+
+Duration$Emergence <- as.factor(Duration$Emergence)
+Duration$Legend <- as.factor(Duration$Legend)
+
+Duration_Bimodal <- Duration %>%  filter(Type=="Bimodal")
+
+p1 <- ggplot(Duration_Bimodal, aes(y = Percentage*100, x = Emergence, group=Legend))+
+  geom_point(aes(shape = Legend), size = 4)+
+  geom_errorbar(aes(ymin=Percentage*100-SD, ymax=Percentage*100+SD), width=.2,
+  ) +
+  theme_classic() +
+  labs(x = expression(paste("Emergence time (", italic("e"), ")")), y = "Percentage of simulations")+
+  theme(text = element_text(size = 17)) +
+  theme(legend.position = "none")
+
+Duration_Early <- Duration %>%  filter(Type=="Early")
+
+p2 <- ggplot(Duration_Early, aes(y = Percentage*100, x = Emergence, group=Legend))+
+  geom_point(aes(shape = Legend), size = 4)+
+  geom_errorbar(aes(ymin=Percentage*100-SD, ymax=Percentage*100+SD), width=.2,
+  ) +
+  theme_classic() +
+  labs(x = expression(paste("Emergence time (", italic("e"), ")")), y = "")+
+  theme(text = element_text(size = 17)) +
+  theme(legend.position = "none")
+
+Duration_Late <- Duration %>%  filter(Type=="Late")
+p3 <- ggplot(Duration_Late, aes(y = Percentage*100, x = Emergence, group=Legend))+
+  geom_point(aes(shape = Legend), size = 4)+
+  geom_errorbar(aes(ymin=Percentage*100-SD, ymax=Percentage*100+SD), width=.2,
+  ) +
+  theme_classic() +
+  labs(x = expression(paste("Emergence time (", italic("e"), ")")), y = "")+
+  theme(text = element_text(size = 17)) +
+  theme(legend.position = "none")
+
+fplot <- p1 + p2 + p3 + plot_annotation(tag_levels = 'A')
+fplot
+
+
+# Figure S 2: Temporal niches in the sub-populations, depending on the timing of adult emergence
+
+
+Phenology <- read_excel("Supplementary_data.xlsx", sheet = 'Uni_vs_Bimodal')
+
+yexpression <- (expression(atop("Average reproductive", paste("activity time ( ",bar("h"["a"])," )"))))
+
+p1 <- ggplot(data=Phenology)+
+  geom_abline(intercept = 0, size=1.5, linetype = "dashed", colour="gray")+
+  geom_point(data=Phenology, size=4,shape=16, colour="darkolivegreen3",aes(x=E,y=Uni))+
+  geom_line(data=Phenology, size=1, colour="darkolivegreen3",aes(x=E,y=Uni))+
+  geom_errorbar(aes(E,ymin=Uni_min, ymax=Uni_max), width = 0.02) +
+  xlab(expression(paste("Emergence time (",italic(e), ")"))) + ylab(yexpression) +
+  theme_classic()+
+  theme(text = element_text(size = 17))
+
+
+p2 <- ggplot(data=Phenology)+
+  geom_abline(intercept = 0, size=1.5, linetype = "dashed", colour="gray")+
+  geom_point(data=Phenology, size=4,shape=15, colour="#00bfc4",aes(x=E,y=Bi_Late))+
+  geom_point(data=Phenology, size=4,shape=17, colour="#F8766D",aes(x=E,y=Bi_Early))+
+  geom_line(data=Phenology, size=1, colour="#00bfc4",aes(x=E,y=Bi_Late))+
+  geom_line(data=Phenology, size=1, colour="#F8766D",aes(x=E,y=Bi_Early))+
+  geom_errorbar(aes(E,ymin=Bi_Late_min, ymax=Bi_Late_max), width = 0.02) +
+  geom_errorbar(aes(E,ymin=Bi_Early_min, ymax=Bi_Early_max), width = 0.02) +
+  xlab(expression(paste("Emergence time (",italic(e), ")"))) + ylab(" ")+
+  theme_classic()+
+  theme(text = element_text(size = 17))
+
+
+final_plot <- p1 + p2 + plot_annotation(tag_levels = 'A')
+final_plot
+
+
+# Figure S 3: Effect of the proportion of loci required to generate incompatibility (G) between individuals on the evolution of sub-populations with different temporal niches.
+
+G <- read_excel("Supplementary_data.xlsx", sheet = 'G_histogram')
+
+G$G <- as.factor(G$G)
+
+ggplot(data=G, aes(G))+
+  geom_bar(aes(weight = BM_G))+
+  xlab("G") + ylab("Percentage of bimodal distributions\nafter 500 generations")+
+  geom_errorbar(aes(ymin=Gymin, ymax=Gymax), width=.2)+
+  theme_classic()+
+  theme(text = element_text(size = 17))
+
+
+# Figure S 4: Effect of the emergence time e on the average timing of reproductive activities in a seasonal model (i.e. assuming non-overlapping generations)
+
+G <- read_excel("Supplementary_data.xlsx", sheet = 'Seasonal_ancestral')
+
+ggplot(data=S, aes(x=Emerg, y=Activity))+
+  geom_line(size=1)+
+  geom_point(size=3)+
+  geom_abline(intercept=0,slope=1,size=1,linetype="dotted")+
+  scale_fill_grey() +
+  geom_errorbar(aes(ymin=ymin, ymax=ymax), width=.05)+
+  ylab("Average sexual activity time")+
+  xlab("Emergence time")+
+  theme_classic()+
+  theme(text = element_text(size = 17))
+
+# Figure S 5: Effect of low levels of male-male competition on the emergence of bimodal distribution in the activity time, in a seasonal model
+
+Season <- read_excel("Supplementary_data.xlsx", sheet = 'Season_low_competition')
+
+Season$dltc <- as.factor(Season$dltc)
+
+ggplot(data=Season, aes(dltc))+
+  geom_point(size=3, aes(x=dltc,y=bimodality))+
+  ylab("Percentage of bimodal distribitions\nafter 500 generations")+
+  xlab(expression("Strength of male-male competition (" * italic(delta["c"]) * ")"))+
+  ylim(0,100)+
+  geom_errorbar(aes(ymin=bimodality-sd*100, ymax=bimodality+sd*100), width=.2) +
+  theme_classic()+
+  theme(text = element_text(size = 17))
+
+
+# Figure S 6: Effect of male-male competition on the evolution of differentiated sub-populations in a seasonal model
+
+
+Season_high_dltc  <- read_excel("Supplementary_data.xlsx", sheet = 'Season_high_competition')
+
+p1 <- ggplot(Season_high_dltc, aes(x=dlt_c, y=BM*100)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=ymin*100, ymax=ymax*100), width=.05,
+                position=position_dodge(.2)) +
+  ylab("Percentage of bimodal distribitions\nafter 500 generations")+
+  xlab(expression(delta*"c"))+
+  theme_classic()+
+  theme(text = element_text(size = 17))
+
+p2 <- ggplot(Season_high_dltc, aes(x=dlt_c, y=Latepos)) +
+  geom_point() +
+  geom_errorbar(aes(ymin=ymn, ymax=ymx), width=.05,
+                position=position_dodge(.2)) +
+  ylab("Average activity time of the late\npopulation after 500 generations")+
+  xlab(expression(delta*"c"))+
+  theme_classic()+
+  theme(text = element_text(size = 17))
+
+fplot <- p1 + p2 + plot_annotation(tag_levels = "A")
+fplot
+
